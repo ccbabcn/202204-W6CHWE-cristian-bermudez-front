81901e1cbd69e96a005b6b7fbeefca260f17afa2
Cristian Bermúdez <ccbabcn@gmail.com>
2022-05-16 21:51:24 +0200

    implemented bearer authorization on thunk

--- src/redux/thunks/robotsThuks.js
+++ src/redux/thunks/robotsThuks.js
@@ -8,7 +8,12 @@ import {
 export const loadRobotsThunk = () => async (dispatch) => {
   try {
     const { data: robots, status } = await axios.get(
-      process.env.REACT_APP_API_URL
+      process.env.REACT_APP_API_URL,
+      {
+        headers: {
+          authorization: `Bearer ${process.env.REACT_APP_CRM_API_ACCESS_TOKEN}`,
+        },
+      }
     );
     if (status === 200) {
       dispatch(loadRobotsActionCreator(robots.robots));
@@ -19,7 +24,12 @@ export const loadRobotsThunk = () => async (dispatch) => {
 export const deleteRobotThunk = (robotId) => async (dispatch) => {
   try {
     const { status } = await axios.delete(
-      `${process.env.REACT_APP_API_URL}delete/${robotId}`
+      `${process.env.REACT_APP_API_URL}delete/${robotId}`,
+      {
+        headers: {
+          authorization: `Bearer ${process.env.REACT_APP_CRM_API_ACCESS_TOKEN}`,
+        },
+      }
     );
     if (status === 200) {
       dispatch(deleteRobotActionCreator(robotId));
